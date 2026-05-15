a7c1e5638bb2df273545c43fee78a0a432b052d7
Marcelo Perez <jhkk1408@gmail.com>
2026-05-15 17:13:06 -0300

    Neue .js-Datei hinzugefügt

---
+++ index5.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const server = express();
+
+// Query params = ?nome=NodeJS
+// Route Params = /curso/2
+// Request Body = { nome: 'Nodejs', tipo: 'Backend' }
+
+// localhost:3000/curso
+server.get('/curso', (req, res) => {
+  const nome = req.query.nome;
+
+  return res.json({ curso: `Aprendendo ${nome}` });
+})
+
+server.listen(3000);
